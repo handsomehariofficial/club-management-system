554f507e0764d7e4f02f9da5f5845d219a78747f
HARIPRASATH A S <120964696+handsomehariofficial@users.noreply.github.com>
2025-08-18 19:51:41 +0530

    Update hh.java

--- CMS - Copy - Copy (2)/hh.java
+++ CMS - Copy - Copy (2)/hh.java
@@ -31,5 +31,37 @@ class largest{
             for (int j=0;j<=2;i++){
         System.err.print("*");
         }
+            import java.util.Arrays;
+class largest{
+    
+    }
+    public static void main(String args[])
+    {
+        int[] aa = {1,3,2,4,1};
+        largest l = new largest();
+        int[] s = l.lgest(aa);
+        System.out.println(l);
+        
+        sop
+            for (int j=0;j<=2;i++){
+        System.err.print("*");
+        }
             
+        
+        
+import java.util.Arrays;
+class largest{
+    
+    }
+    public static void main(String args[])
+    {
+        int[] aa = {1,3,2,4,1};
+        largest l = new largest();
+        int[] s = l.lgest(aa);
+        System.out.println(l);
+        
+        sop
+            for (int j=0;j<=2;i++){
+        System.err.print("*");
+        }
 
